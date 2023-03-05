b26f3a52f5a75d2ea871bd7676e2e5e05b2f3ff1
tapash22 <tapasp263@gmail.com>
2023-03-05 23:51:37 +0600

    add form and use css with module

--- src/Form.js
+++ src/Form.js
@@ -1,39 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 
-class Form extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {value: '',value1:''};
-  
-      this.handleChange = this.handleChange.bind(this);
-      this.handleSubmit = this.handleSubmit.bind(this);
-    }
-  
-    handleChange(e) {
-      this.setState({value: e.target.value, value1:e.target.value1});
-    }
-  
-    handleSubmit(e) {
-      alert('A name was submitted: ' + this.state.value +'text'+this.state.value1);
-      e.preventDefault();
-    }
-  
-    render() {
-      return (
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Name:</label><br/>
-            <input type="text" value={this.state.value} onChange={this.handleChange} />
-
-            <label>Message</label><br/>
-            <textarea value={this.state.value1} onChange={this.handleChange}>
-                <br/>
-            </textarea>
-          <br/>
-          <button type="submit" >submit</button>
-        </form>
-      );
-    }
+function Form() {
+  const [inputs, setInputs] = useState({});
+  function handleForm(e) {
+    const name = e.target.name;
+    const value = e.target.value;
+    setInputs((values) => ({ ...values, [name]: value }));
   }
+  function submitForm(e) {
+    e.preventDefault();
+    console.log(inputs);
+  }
+  return (
+    <form onSubmit={submitForm}>
+      <input
+        placeholder="name"
+        name="name"
+        type="text"
+        required
+        value={inputs.name || ""}
+        onChange={handleForm}
+      />
+      <input
+        placeholder="Email"
+        name="email"
+        type="email"
+        required
+        value={inputs.email || ""}
+        onChange={handleForm}
+      />
+      <input
+        placeholder="Password"
+        name="pass"
+        value={inputs.pass || ""}
+        onChange={handleForm}
+        type="password"
+      />
+      <textarea
+        placeholder="address"
+        name="adderss"
+        value={inputs.adderss || ""}
+        onChange={handleForm}
+        type="text"
+      ></textarea>
+      <input type="submit" value="DONE" />
+    </form>
+  );
+}
 
-  export default Form;
+export default Form;
